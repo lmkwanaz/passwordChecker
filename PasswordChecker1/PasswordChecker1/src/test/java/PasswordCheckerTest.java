55252a7ab76fe95aba50bdf54d8e54f47ec3aaf7
HlobisileMlebuka <mlebukah@gmail.com>
2020-01-15 15:57:58 +0200

    updated

---
+++ PasswordChecker1/PasswordChecker1/src/test/java/PasswordCheckerTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordCheckerTest {
+
+    @Test
+    void passwordIsValid() {
+        PasswordChecker pas = new PasswordChecker();
+    }
+
+    @Test
+    void passwordIsOk() {
+    }
+}
